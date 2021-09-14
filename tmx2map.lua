ddb39f256b0084a073a5e32d64f18c6f446040c4
Nathan Jent <nathanjent@gmail.com>
2021-09-13 21:52:57 -0400

    Fix some of the unit tests

--- tmx2map.lua
+++ tmx2map.lua
@@ -3,9 +3,113 @@ local handler = require("xmlhandler.tree")
 local base64 = require("base64")
 local zlib = require("zlib")
 local zstd = require("zstd")
+
+-- TIC-80 map size
 local SIZE = 32640
 
 local Tmx2Map = {}
+
+local function parseCSV(csvtiledata)
+    local tiledata = {}
+    local i = 1
+    for value in csvtiledata:gmatch("([^,%s]+)") do
+        tiledata[i] = value - 1
+        i = i + 1
+    end
+    return tiledata
+end
+
+local function test_TmxCsvToMap()
+    local expected = {
+        1,2,3,4,
+        2,4,5,6,
+        3,5,6,7,
+        4,6,7,8
+    }
+
+    local actual = parseCSV([[
+        2,3,4,5,
+        3,5,6,7,
+        4,6,7,8,
+        5,7,8,9
+        ]])
+
+    for i, e in ipairs(expected) do
+        assert(e == actual[i], "Unmatch value at index ["..i.."] "..e.." <> "..actual[i])
+    end
+end
+
+-- Parse the deprecated XML tile layer format
+local function processXMLtiledata(xmltiledata, size)
+    local tiledata = {}
+    for i=1,size do
+        tiledata[i] = xmltiledata.tile[i]._attr.gid - 1
+    end
+    return tiledata
+end
+
+local function test_TmxXmlToMap()
+    local expected = {
+        1,2,3,4,
+        2,4,5,6,
+        3,5,6,7,
+        4,6,7,8
+    }
+
+    local xml = [[
+    <data>
+        <tile gid="2"/>
+        <tile gid="3"/>
+        <tile gid="4"/>
+        <tile gid="5"/>
+        <tile gid="3"/>
+        <tile gid="5"/>
+        <tile gid="6"/>
+        <tile gid="7"/>
+        <tile gid="4"/>
+        <tile gid="6"/>
+        <tile gid="7"/>
+        <tile gid="8"/>
+        <tile gid="5"/>
+        <tile gid="7"/>
+        <tile gid="8"/>
+        <tile gid="9"/>
+    </data>
+    ]]
+
+    xml2lua.parser(handler):parse(xml)
+
+    local actual = processXMLtiledata(handler.root.data, 16)
+
+    for i, e in ipairs(expected) do
+        assert(e == actual[i], "Unmatch value at index ["..i.."] "..e.." <> "..actual[i])
+    end
+end
+
+local function parseBase64(base64tiledata, compression)
+    local decodeddata = base64.decode(base64tiledata)
+
+    -- Decompress if compression type provided
+    if compression then
+        if compression == "gzip"
+            or compression == "zlib" then
+            decodeddata = zlib.inflate()(decodeddata)
+        elseif compression == "zstd" then
+            decodeddata = zstd.decompress(decodeddata)
+        end
+    end
+
+    local bytedata = { decodeddata:byte(1, -1) }
+    local tiledata = {}
+
+    -- Step over the extra bytes from 32 bit data
+    -- The TIC-80 map is not big enough to use the extra bytes
+    for i=1,#bytedata,4 do
+        table.insert(tiledata,  bytedata[i] - 1)
+    end
+    return tiledata
+end
+
 -- Converts a Tiled TMX file to a TIC-80 map file
 function Tmx2Map:convert(xml)
     local parser = xml2lua.parser(handler)
@@ -14,103 +118,16 @@ function Tmx2Map:convert(xml)
     local data = handler.root.map.layer.data
     if data._attr then
         if data._attr.encoding == "csv" then
-            local tiledata = {}
-            local i = 1
-            for value in data[1]:gmatch("([^,%s]+)") do
-                tiledata[i] = value - 1
-                i = i + 1
-            end
+            local tiledata = parseCSV(data[1])
             return string.char(table.unpack(tiledata))
         elseif data._attr.encoding == "base64" then
-            local decodeddata = base64.decode(data[1])
-
-            -- Decompress if compression type provided
-            if data._attr.compression then
-                if data._attr.compression == "gzip"
-                    or data._attr.compression == "zlib" then
-                    decodeddata = zlib.inflate()(decodeddata)
-                elseif data._attr.compression == "zstd" then
-                    decodeddata = zstd.decompress(decodeddata)
-                end
-            end
-
-            local bytedata = { decodeddata:byte(1, -1) }
-            local tiledata = {}
-
-            -- Step over the extra bytes from 32 bit data
-            -- The TIC-80 map is not big enough to use the extra bytes
-            for i=1,#bytedata,4 do
-                table.insert(tiledata,  bytedata[i] - 1)
-            end
-
+            local tiledata = parseBase64(data[1], data._attr.compression)
             return string.char(table.unpack(tiledata))
         end
     else
-        -- Deprecated XML tile layer format
-        local tiledata = {}
-        for i=1,SIZE do
-            tiledata[i] = data.tile[i]._attr.gid - 1
-        end
+        local tiledata = processXMLtiledata(data, SIZE)
         return string.char(table.unpack(tiledata))
     end
-end
-
-local function loadTestMap()
-    local mapfile, e = io.open("./testassets/test.map", "rb")
-    if mapfile then
-        local mapdata = mapfile:read("*a")
-        mapfile:close()
-        return mapdata
-    end
-    error(e)
-end
-
-local function loadTestTmx(encoding, compression)
-    local testfile = ""
-    local filenamepart1 = "./testassets/test_"
-    local filenamepart2 = "_layerdata.tmx"
-    if not encoding then
-        -- Deprecated XML
-        testfile = filenamepart1.."xml"..filenamepart2
-    elseif not compression then
-        -- Uncompressed
-        testfile = filenamepart1..encoding..filenamepart2
-    else
-        -- Compressed
-        testfile = filenamepart1..encoding..compression..filenamepart2
-    end
-    print("Testing conversion of "..testfile)
-    return xml2lua.loadFile(testfile)
-end
-
-local function test_TmxXmlToMap()
-    local expectedmapdata = loadTestMap()
-    local tmxxml = loadTestTmx()
-
-    -- run conversion
-    local actualmapdata = Tmx2Map:convert(tmxxml)
-
-    assert(expectedmapdata == actualmapdata, "Tile data mismatch")
-end
-
-local function test_TmxCsvToMap()
-    local expectedmapdata = loadTestMap()
-    local tmxxmlcsv = loadTestTmx("csv")
-
-    -- run conversion
-    local actualmapdata = Tmx2Map:convert(tmxxmlcsv)
-
-    assert(expectedmapdata == actualmapdata, "Tile data mismatch")
-end
-
-local function test_TmxBase64ToMap()
-    local expectedmapdata = loadTestMap()
-    local tmxxmlbase64 = loadTestTmx("base64")
-
-    -- run conversion
-    local actualmapdata = Tmx2Map:convert(tmxxmlbase64)
-
-    assert(expectedmapdata == actualmapdata, "Tile data mismatch")
 end
 
 return Tmx2Map
