a046772843c3acaad94436df96fbc80593e9758a
Nathan Jent <nathanjent@gmail.com>
2021-09-19 22:19:50 -0400

    Update to create executable module

---
+++ ticmap2tmx/init.lua
@@ -0,0 +1,34 @@
+local xml2lua = require("xml2lua")
+local tmx2map = require("ticmap2tmx.tmx2map")
+local map2tmx = require("ticmap2tmx.map2tmx")
+
+local Ticmap2tmx = {}
+
+function Ticmap2tmx:convert(fromfilename, tofilename)
+    print(fromfilename.."-->"..tofilename)
+
+    local fromext = fromfilename:match("[^.]+$")
+    local toext = tofilename:match("[^.]+$")
+
+    if fromext == "tmx" and toext == "map" then
+        local xml = xml2lua.loadFile(fromfilename)
+        local output = tmx2map:convert(xml)
+        local tofile  = io.open(tofilename, "wb")
+        tofile:write(output)
+        tofile:close()
+        print("Converted TMX to MAP.")
+    elseif fromext == "map" and toext == "tmx" then
+        local fromfile = io.open(fromfilename, "rb")
+        local input = fromfile:read("a")
+        local output = map2tmx:convert(input)
+        local tofile  = io.open(tofilename, "w")
+        tofile:write(output)
+        tofile:close()
+        fromfile:close()
+        print("Converted MAP to TMX.")
+    else
+        error("File extensions must be 'tmx'->'map' or 'map'->'tmx' only.")
+    end
+end
+
+return Ticmap2tmx
